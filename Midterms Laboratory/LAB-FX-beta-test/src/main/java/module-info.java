33267a79a3a9a2c7c38dd3a4ce76049f459bb450
reeaash <117620183+AAlimurung@users.noreply.github.com>
2024-03-18 19:19:40 +0800

    Delete Midterms Laboratory directory

--- Midterms Laboratory/LAB-FX-beta-test/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.labfxbetatest {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.labfxbetatest to javafx.fxml;
-    exports com.example.labfxbetatest;
-}
--- Midterms Laboratory/alimurung_perfect_number/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.alimurung_perfect_number {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.alimurung_perfect_number to javafx.fxml;
-    exports com.example.alimurung_perfect_number;
-}
